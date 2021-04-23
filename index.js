0d550b8309cfd4751cfb62d029afada96515acfd
mariocab98 <yankeescab@gmail.com>
2021-04-22 22:30:09 -0400

    added all classes

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const fs = require("fs");
+
+const employee = require("./lib/Employee");
+const manager = require("./lib/Manager");
+const intern = require("./lib/Intern");
+const engineer = require("./lib/Engineer");
+
+let team = [];
+
+function generateTeam() {}
