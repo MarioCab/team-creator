f03e718085ccd7a988dbac8c5fab75ced298af04
mariocab98 <yankeescab@gmail.com>
2021-04-23 11:18:58 -0400

    html sucessfully generated

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
+const path = require("path");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
@@ -159,6 +163,7 @@ function endQuestion() {
         startQuestion();
       } else {
         console.log(generateHTML(team));
+        fs.writeFileSync(outputPath, generateHTML(team), "utf-8");
       }
     });
 }
