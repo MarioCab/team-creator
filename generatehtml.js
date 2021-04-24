f83f348cc950f151ea3f4e4f1336b9885cc85929
Mario Cabrera <yankeescab@gmail.com>
2021-04-24 12:42:06 -0400

    still working on getting links

--- generatehtml.js
+++ generatehtml.js
@@ -1,51 +1,32 @@
 function generateHTML(team) {
   return `
   <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
-      crossorigin="anonymous"
-    />
-    <link href="/style.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="container-fluid" id="header">
-      <h1>Team</h1>
-    </div>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+        crossorigin="anonymous"
+      />
+      <link href="/style.css" rel="stylesheet" />
+    </head>
+    <body>
+      <div class="container-fluid" id="header">
+        <h1>Team</h1>
+      </div>
   
-    <div class="container">
-    <div class="row row-cols-3">
-      <div class="col">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title"${employee.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${employee.role}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">${employee.email}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">${employee.id}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">${employee.githubName}${employee.school}${employee.officeNumber}</h6>
-            <p class="card-text">
-              ${employee.task}
-            </p>
-          </div>
+      <div class="container">
+        <div class="row row-cols-3">
+          ${generateCards(team)}
         </div>
       </div>
-    </div>
-  </div>
-
-
-
-
-
-
-  </body>
-</html>
+    </body>
+  </html>
 `;
 }
 
@@ -53,24 +34,20 @@ function generateCards(team) {
   let array = [];
 
   team.forEach((employee) => {
-    array.push(`<div class="container">
-    <div class="row row-cols-3">
-      <div class="col">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title"${employee.name}</h5>
+    array.push(`
+  <div class="col">
+    <div class="card" id="cards" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">${employee.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${employee.role}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">${employee.email}</h6>
+            <h6 class="card-subtitle mb-2 text-muted"><a href="mailto:${employee.email}">${employee.email}</a></h6>
             <h6 class="card-subtitle mb-2 text-muted">${employee.id}</h6>
             <h6 class="card-subtitle mb-2 text-muted">${employee.githubName}${employee.school}${employee.officeNumber}</h6>
-            <p class="card-text">
-              ${employee.task}
-            </p>
-          </div>
+            <p class="card-text">${employee.task}</p>
         </div>
-      </div>
     </div>
-  </div>`);
+  </div>
+  `);
   });
 
   return array.join("");
